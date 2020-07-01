95b0a6cd45f20c5721bf04fd6053c7aef6758b05
Speedway <jailsonjpo@gmail.com>
2020-07-01 20:58:42 -0300

    pc - configuracoes - camera

--- app/src/main/java/com/jailsonspeedway/whatsapp/helper/Permissao.java
+++ app/src/main/java/com/jailsonspeedway/whatsapp/helper/Permissao.java
@@ -1,8 +1,10 @@
 package com.jailsonspeedway.whatsapp.helper;
 
 import android.app.Activity;
+import android.content.pm.PackageManager;
 import android.os.Build;
 
+import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 
 import java.util.ArrayList;
@@ -10,7 +12,7 @@ import java.util.List;
 
 public class Permissao {
 
-    public static boolean validarPermissoes(String[] permissoes, Activity activity){
+    public static boolean validarPermissoes(String[] permissoes, Activity activity, int requestCode){
 
         if(Build.VERSION.SDK_INT >= 23){
 
@@ -18,8 +20,19 @@ public class Permissao {
             // Percorrer as permissões passadas, verificando uma a uma, se ja tem a permissão liberada
 
             for(String permissao : permissoes){
-                ContextCompat.checkSelfPermission(activity, permissao);
+                Boolean temPermissao =  ContextCompat.checkSelfPermission(activity, permissao) == PackageManager.PERMISSION_GRANTED;
+
+                if(!temPermissao) listaPermissoes.add(permissao);
             }
+
+            //Caso a lista esteja vazia, não é necessário solicitar permissao
+            if(listaPermissoes.isEmpty())
+                return true;
+           String[] novasPermissoes = new String[listaPermissoes.size()];
+           listaPermissoes.toArray(novasPermissoes);
+
+            //Solicita permissao
+            ActivityCompat.requestPermissions(activity, novasPermissoes, requestCode);
 
         }
 
